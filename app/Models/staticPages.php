2588a782a1eb0347e7d06554cee2704e0765c177
Gursharan Singh <gursharan@vsrkcapital.com>
2022-07-02 12:27:31 +0530

    added update logo logic to remove error for not exiting images

--- app/Models/staticPages.php
+++ app/Models/staticPages.php
@@ -21,7 +21,7 @@ class staticPages extends Model
     }
 
     public static function getAllFields($page){
-        $raw_data = self::where('page',$page)->get(); 
+        $raw_data = self::where('page',$page)->get();
         $data = [];
         foreach($raw_data as $raw){
             $data[$raw->field_name] = $raw->field_value;
@@ -34,11 +34,11 @@ class staticPages extends Model
     public static function updateFields($page, $request){
 
         foreach($request->all() as $key=>$value){
-      
+
             if(in_array($key,['_token','_method','submit'])){
                 continue;
             }
-     
+
            $value = is_string($value)?$value:json_encode($value);
 
             $staticPage = self::where('page',$page)
@@ -46,20 +46,21 @@ class staticPages extends Model
                                 ->first();
 
             if($staticPage){
-               
+
                 if($key == 'website_logo' && $request->file('website_logo')){
-                        $value  = updateMedia($staticPage->field_value, $request->file('website_logo'),'GD');
+                        $value  = !empty($staticPage->field_value) ? updateMedia($staticPage->field_value, $request->file('website_logo'),'GD'): addMedia($request->file('website_logo'),'GD');
                 }
                 elseif($key == 'favicon' && $request->file('favicon')){
-                        $value  = updateMedia($staticPage->field_value, $request->file('favicon'),'GD');
+                        $value  = !empty($staticPage->field_value) ? updateMedia($staticPage->field_value, $request->file('favicon'),'GD') : addMedia($request->file('favicon'),'GD');
                     }
             }
             else{
                 $staticPage = new self;
                 $staticPage->page = $page;
                 $staticPage->field_name = $key;
-               
+
                 if($key == 'website_logo' && $request->file('website_logo')){
+
                     $value  = addMedia($request->file('website_logo'),'GD');
                 }
                 elseif($key == 'favicon' && $request->file('favicon')){
