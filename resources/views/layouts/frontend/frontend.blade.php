ef90d2fb73eda9130e6acea6a5076ea6d1399cbd
Gursharan Singh <gursharan@vsrkcapital.com>
2022-07-08 18:13:50 +0530

    bootstrap remove cdn

--- resources/views/layouts/frontend/frontend.blade.php
+++ resources/views/layouts/frontend/frontend.blade.php
@@ -5,7 +5,7 @@
     <title>{{@$gd->website_title}}</title>
     <link rel="icon" href="{{asset(@$gd->favicon)}}" type="image/png" sizes="16x16" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/helper.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('frontend/css/style.css')}}">
